45f9401ff4aa7c2d5baaf6bfc27b956ad0480e4c
vuong2k2002@gmail.com <vuong2k2002@gmail.com>
2024-10-06 20:32:37 +0700

    no message

--- nestjs-api-ecm/src/app.service.ts
+++ nestjs-api-ecm/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Nguyen Van Tuyen!';
+    return 'Hello Nguyen Van Tuyen xau trai!';
   }
 }
