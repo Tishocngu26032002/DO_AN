1dce284848105a963c8f51198e9f1a419b172178
thanhleee <tle58440@gmail.com>
2024-10-11 14:01:58 +0700

    layout-web

--- reactjs-ecm/src/main.jsx
+++ reactjs-ecm/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from './components/HomePages/homepage.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/homepage" replace />} /> 
+                <Route path="/homepage" element={<HomePage />} /> 
+               
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
