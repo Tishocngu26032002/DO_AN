b06a7c3146356264b815818c1cd289d1f7ca7fe5
vuVinh525 <vuquangvinh535@gmail.com>
2024-10-19 14:07:12 +0700

    giao dien ShopGrid

--- reactjs-ecm/src/main.jsx
+++ reactjs-ecm/src/main.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import HomePage from './components/HomePages/homepage.jsx';
+import ShopGrid from './components/ShopGrid/ShopGrid.jsx';
+import './index.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+rootElement.style.width = "100%";
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigate to="/homepage" replace />} /> 
-                <Route path="/homepage" element={<HomePage />} /> 
-               
+                <Route path="/" element={<Navigate to="/products" replace />} />
+                <Route path="/products" element={<ShopGrid />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
--- reactjs-ecm/tailwind.config.js
+++ reactjs-ecm/tailwind.config.js
@@ -3,10 +3,23 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    // "./src/***/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        tablet: { max: "799px" },
+        // => @media (max-width: 799px) { ... }
+        mobile: { max: "477px" },
+        // => @media (max-width: 477px) { ... }
+      },
+    },
+    screens: {
+      sm: "576px",
+      md: "799px",
+      lg: "992px",
+      xl: "1200px",
+    },
   },
   plugins: [],
-}
-
+};
