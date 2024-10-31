45564a94058b4e4e202aed54833bc6384db155de
thanhleee <tle58440@gmail.com>
2024-10-31 10:20:11 +0700

    update UI mn category,user,order, payment,cart,productdetail

--- reactjs-ecm/src/components/Admin/AdminHeader/admin-header.jsx
+++ reactjs-ecm/src/components/Admin/AdminHeader/admin-header.jsx
@@ -10,16 +10,16 @@ function HeaderAdmin() {
   };
 
   return (
-    <div className="sticky top-0 z-50 flex flex-col items-end bg-[#225a3e] px-12 py-3 shadow-lg shadow-custom-dark">
+    <div className="sticky top-0 z-50 flex  bg-[#225a3e] px-12 py-3 shadow-lg shadow-custom-dark">
       {/* Logo */}
-      <a href="/home-page" className="-mt-1 absolute left-10">
+      <a href="/home-page" className="-mt-1 absolute right-[70px]">
         <img
           src="images/Crops organic farm.png"
           className="rounded-[0_20px_20px_20px] hover:rounded-[20px_20px_0_20px] shadow-custom-dark transition-all duration-500 ease-in-out p-1 w-20 h-[85px] md:w-[90px] md:h-[95px] bg-white fadeInUp md:border-0 border-2 border-[#006633]"
           alt="Logo"
         />
       </a>
-      <div className="flex items-center -mt-5 pt-6">
+      <div className="flex items-center -mt-5 pt-6  left-10">
         <button id="bar" className="px-4" onClick={toggleMenu}>
           {isMenuOpen ? (
             <IoClose className="h-[35px] w-[35px] text-white" aria-hidden="true" />
@@ -31,7 +31,7 @@ function HeaderAdmin() {
 
       {/* Sidebar Menu */}
       <div
-        className={`fixed top-0 left-0 z-50 h-full w-[250px] bg-white shadow-lg transform transition-transform duration-300 ease-in-out border-r-2 border-[#006532] ${
+        className={`fixed top-[63px] left-0 z-50 h-full w-[250px] bg-white shadow-lg transform transition-transform duration-300 ease-in-out border-r-2 border-[#006532] ${
           isMenuOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
@@ -46,6 +46,18 @@ function HeaderAdmin() {
 
         {/* Navigation Links */}
         <ul className="flex flex-col items-start text-[#006532] font-semibold">
+          <li className="w-full px-6 py-4 border-b border-[#006532] hover:bg-[#80c9a4] hover:text-white transition-colors duration-300">
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-[#006532] border-l-4 border-[#006532] pl-2"
+                  : "text-[#006532] hover:text-white"
+              }
+            >
+              Dashboard
+            </NavLink>
+          </li>
           <li className="w-full px-6 py-4 border-b border-[#006532] hover:bg-[#80c9a4] hover:text-white transition-colors duration-300">
             <NavLink
               to="/manage-user"
@@ -106,18 +118,7 @@ function HeaderAdmin() {
               Manage Supplier
             </NavLink>
           </li>
-          <li className="w-full px-6 py-4 border-b border-[#006532] hover:bg-[#80c9a4] hover:text-white transition-colors duration-300">
-            <NavLink
-              to="/dashboard"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-[#006532] border-l-4 border-[#006532] pl-2"
-                  : "text-[#006532] hover:text-white"
-              }
-            >
-              Dashboard
-            </NavLink>
-          </li>
+          
         </ul>
       </div>
     </div>
